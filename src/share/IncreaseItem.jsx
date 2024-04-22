c69db0f9094a5cbafb7408c1b64dcf169cca975e
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2024-04-22 23:54:17 +0330

    update modal need to fixed more

--- src/share/IncreaseItem.jsx
+++ src/share/IncreaseItem.jsx
@@ -1,30 +1,36 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 
 const IncreaseItem = ({ title, image, cost }) => {
   return (
-    <Grid
-      container
-      display='flex'
-      justifyContent={"space-between"}
-      alignItems='center'
-      alignContent={"center"}>
-      <Grid item xs={10} sm={10} md={10} lg={10}>
-        <Box>
-          <img src={image} />
-        </Box>
-        <Box display='flex' flexDirection={"column"}>
-          <Typography variant='h6'>{title}</Typography>
-          <Typography variant='h6' fontWeight='bold'>
-            {cost}
-          </Typography>
-        </Box>
+    <Container>
+      <Grid
+        container
+        display='flex'
+        justifyContent={"space-between"}
+        alignItems='center'
+        alignContent={"center"}
+        bgcolor='rgb(237, 252, 255)'
+        m='2rem 0 0 2rem '
+        borderRadius={"16px"}
+        p={2}>
+        <Grid item xs={10} sm={10} md={10} lg={10}>
+          <Box>
+            <img src={image} />
+          </Box>
+          <Box display='flex' flexDirection={"column"}>
+            <Typography variant='h6'>{title}</Typography>
+            <Typography variant='h6' fontWeight='bold'>
+              {cost}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2}>
+          <Button>+</Button>
+          <span>0</span>
+          <Button>-</Button>
+        </Grid>
       </Grid>
-      <Grid item xs={2} sm={2} md={2} lg={2}>
-        <Button>+</Button>
-        <span>0</span>
-        <Button>-</Button>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
 
--- src/share/ModalIncrease.jsx
+++ src/share/ModalIncrease.jsx
@@ -1,37 +1,60 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 import IncreaseItem from "./IncreaseItem";
 
 const ModalIncrease = () => {
   return (
-    <Container
-      sx={{ zIndex: "1", position: "fixed", bottom: "0", bgcolor: "silver" }}>
-      <Grid container>
-        <Grid item xs={12} sm={12} md={12} lg={12}>
-          <Typography variant='h5'>کت و شلوار</Typography>
+    <>
+      <Box
+        bgcolor='rgba(0,0,0,0.8)'
+        width={"100%"}
+        height={"100%"}
+        zIndex={"1"}
+        position={"absolute"}
+        top={"0"}
+        right={"0"}
+      />
+      
+        <Container sx={{
+          position: "fixed",
+m:"0 1.6rem",          zIndex: "1",
+          bottom: "0",
+          bgcolor: "white",
+          borderRadius: "16px 16px 0 0",
+width:"1100px"
+        }} >
+
+
+        <Grid container position=''>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography variant='h5' fontWeight={"bold"} m={3}>
+              کت و شلوار
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <IncreaseItem
+              image='-'
+              title={"خشکشویی + اتو"}
+              cost={"۱۵۴,۸۰۰ تومان"}
+            />
+          </Grid>{" "}
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <IncreaseItem
+              image='-'
+              title={"خشکشویی + اتو"}
+              cost={"۱۵۴,۸۰۰ تومان"}
+            />
+          </Grid>{" "}
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <IncreaseItem
+              image='-'
+              title={"خشکشویی + اتو"}
+              cost={"۱۵۴,۸۰۰ تومان"}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12}>
-          <IncreaseItem
-            image='-'
-            title={"خشکشویی + اتو"}
-            cost={"۱۵۴,۸۰۰ تومان"}
-          />
-        </Grid>{" "}
-        <Grid item xs={12} sm={12} md={12} lg={12}>
-          <IncreaseItem
-            image='-'
-            title={"خشکشویی + اتو"}
-            cost={"۱۵۴,۸۰۰ تومان"}
-          />
-        </Grid>{" "}
-        <Grid item xs={12} sm={12} md={12} lg={12}>
-          <IncreaseItem
-            image='-'
-            title={"خشکشویی + اتو"}
-            cost={"۱۵۴,۸۰۰ تومان"}
-          />
-        </Grid>
-      </Grid>
-    </Container>
+
+        </Container>
+    </>
   );
 };
 
